e4655602ea8508bd592f98dd56099e79f9b9b2da
Luciano <lucianotimo@gmail.com>
2022-04-06 22:41:21 -0300

    segundo commit luciano

---
+++ nurb.js
@@ -0,0 +1,14 @@
+for (let i = 0 ; i <= 82 ; i++){
+if (i % 2 !==0 || i < 82)
+{
+    console.log('Curva a la derecha');
+} else if {
+    console.log('Curva a la izquierda');
+} else 
+{console.log('Finish')}};
+
+
+switch (i % 2 !==0 || i < 82){
+    case 0:
+        
+}
